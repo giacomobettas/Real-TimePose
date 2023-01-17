6ea5a0edb3084c18c5a6e0261c70f3be17cd47cf
Giacomo Bettas-Begalin <giacomo.bettas@gmail.com>
2023-01-18 00:05:23 +0100

    fixed input

--- PoseEstimator.py
+++ PoseEstimator.py
@@ -15,18 +15,18 @@ with mp_pose.Pose(
     static_image_mode=True,
     model_complexity=2,
     min_detection_confidence=0.5) as pose:
-    image = cv2.imread('Hatha-Yoga-Poses-Asanas-Sequences.jpg')  #Insert your Image Here
+    image = cv2.imread('test.jpg')  #Insert your Image Here
     image_height, image_width, _  = image.shape
     # Convert the BGR image to RGB before processing.
     results = pose.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     # Draw pose landmarks on the image.
     annotated_image = image.copy()
     mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    cv2.imwrite(r'Hatha-Yoga-Poses-Asanas-Sequences.png', annotated_image)
+    cv2.imwrite(r'test.png', annotated_image)
 
 # For webcam input:
 #cap = cv2.VideoCapture(0)
-cap = cv2.VideoCapture("1.mp4")
+cap = cv2.VideoCapture("test1.avi")
 #For Video input:
 prevTime = 0
 with mp_pose.Pose(
