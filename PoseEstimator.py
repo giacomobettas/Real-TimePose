559cbba8d8b7957ea0fc27acc397abdd00a7b499
Giacomo Bettas-Begalin <giacomo.bettas@gmail.com>
2023-01-25 16:27:20 +0100

    test

--- PoseEstimator.py
+++ PoseEstimator.py
@@ -10,7 +10,7 @@ mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
 mp_holistic = mp.solutions.holistic
 
-# For static images:
+""" # For static images:
 with mp_pose.Pose(
     static_image_mode=True,
     model_complexity=2,
@@ -22,7 +22,7 @@ with mp_pose.Pose(
     # Draw pose landmarks on the image.
     annotated_image = image.copy()
     mp_drawing.draw_landmarks(annotated_image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-    cv2.imwrite(r'test.png', annotated_image)
+    cv2.imwrite(r'test.png', annotated_image) """
 
 # For webcam input:
 #cap = cv2.VideoCapture(0)
